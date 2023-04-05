1cac9909cfcf8641a4fc9781e296a53b18a3d583
Pedro Meruge <a100709@alunos.uminho.pt>
2023-04-05 16:17:47 +0100

    client

--- include/common/defines.h
+++ include/common/defines.h
@@ -1,15 +1,16 @@
+#include <unistd.h>
+#include <sys/types.h>
 #ifndef DEFINES_H
 #define DEFINES_H
-
 #define PIPE_NAME "pipes/pipe"
 
 enum msgType {
-    Start = 0,
-    End = 1
+    START = 0,
+    END = 1
 };
 
 typedef struct procLog {
-	int pid;
+	pid_t pid;
 	char name[100];
 	int time; // mudar tipo depois 
 } procLog;
--- src/tracer/main.c
+++ src/tracer/main.c
@@ -7,13 +7,16 @@
 #include "defines.h"
 #include <fcntl.h>
 #include <sys/time.h>
-
 #define MESSAGE_BUFF 1024
+
+typedef struct Info {
+	enum msgType type;
+	procLog proc;
+} Info;
 
 // abrir e fechar aqui????
 void message_server(char * message, size_t len) {
-	int fd = open(PIPE_NAME, O_RDONLY);
-
+	int fd = open(PIPE_NAME, O_WRONLY); // tirar daqui depois, ja aberto antes?
 	// fazer write em loop???????????????????
 	if (write(fd, message, len) == -1) {
 		perror("Error on write");
@@ -22,25 +25,33 @@ void message_server(char * message, size_t len) {
 	close(fd);
 }
 
-void simple_execute(char **args) {
-	// sem error checking por agora
+void ping_init (int server_d, pid_t pid, char * name) {
+	Info new;
+	new.type = START;
+	new.proc.pid = pid;
+	strcpy(new.proc.name,name);
 	struct timeval current_time;
-	char * message = malloc(sizeof(char) * MESSAGE_BUFF);
+	new.proc.time = gettimeofday(&current_time, NULL);
+        // printf("seconds : %ld\nmicro seconds : %ld",
+        // current_time.tv_sec, current_time.tv_usec);
+        // milis: current_time.tv_usec / 1000
+
+	// char * message = malloc(sizeof(char) * MESSAGE_BUFF);
+	write(server_d,&new,sizeof(Info));
+}
+
+void simple_execute(char **args) {
+	int fd = open(PIPE_NAME, O_WRONLY);
+	// sem error checking por agora
 	if (fork() == 0) {
-		gettimeofday(&current_time, NULL);
-		// printf("seconds : %ld\nmicro seconds : %ld",
-		// current_time.tv_sec, current_time.tv_usec);
-		// milis: current_time.tv_usec / 1000;
-
-
-
+		ping_init(fd,getpid(),args[0]);
 		execvp(args[0], args);
 	}
+	close(fd);
 }
 
 int main (int argc, char **argv) {
 	if (argc < 2) return 1;
-
 
 	// maneira rota de usar flags mas não interessa
 	if (strcmp(argv[1], "execute") == 0) {
@@ -48,6 +59,5 @@ int main (int argc, char **argv) {
 			simple_execute(argv + 3);
 		}
 	}
-
 	return 0;
 }
